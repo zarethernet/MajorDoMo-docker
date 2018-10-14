ac36dc286231f79eea44907915f2718ce25ed543
beresnevdr <beresnev-dr@yandex.ru>
2018-10-14 15:01:02 +0700

    Add install MajorDoMo
    Signed-off-by: beresnevdr <beresnev-dr@yandex.ru>

--- start.sh
+++ start.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 ssh-keygen -A
+chmod 600 /root/.ssh
+chmod 400 /root/.ssh/authorized_keys
 /usr/sbin/sshd -d
+
+# Install MajorDoMo
+cd /root
+git clone https://github.com/sergejey/majordomo.git
+rm -f /var/www/localhost/htdocs/index.html
+cp -rp /root/majordomo/* /var/www/localhost/htdocs
+cp -rp /root/majordomo/.htaccess /var/www/localhost/htdocs
+cp /var/www/localhost/htdocs/config.php.sample /var/www/localhost/htdocs/config.php
+chown -R apache:apache /var/www/localhost/htdocs
+find /var/www/localhost/htdocs -type f -exec sudo chmod 0666 {} \;
+find /var/www/localhost/htdocs -type d -exec sudo chmod 0777 {} \;
