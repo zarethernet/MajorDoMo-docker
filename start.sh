c230a091a0cdb4cf61e3ff0aa62595e60736016f
beresnevdr <beresnev-dr@yandex.ru>
2018-10-14 16:50:06 +0700

    Fix
    Signed-off-by: beresnevdr <beresnev-dr@yandex.ru>

--- start.sh
+++ start.sh
@@ -2,11 +2,4 @@
 ssh-keygen -A
 chmod 600 /root/.ssh
 chmod 400 /root/.ssh/authorized_keys
-#/usr/sbin/sshd -d &
-
-DAEMON=httpd
-EXEC=$(which "$DAEMON")
-
-ARGS="-f /etc/apache2/httpd.conf -D FOREGROUND"
-
-${EXEC} ${ARGS} &
+/usr/sbin/sshd -d
