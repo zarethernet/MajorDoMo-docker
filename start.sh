c78d889ea9b28eb033d459ce7638a79a310dfebb
beresnevdr <beresnev-dr@yandex.ru>
2018-10-14 15:11:21 +0700

    Fix start.sh
    Signed-off-by: beresnevdr <beresnev-dr@yandex.ru>

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@
 ssh-keygen -A
 chmod 600 /root/.ssh
 chmod 400 /root/.ssh/authorized_keys
-/usr/sbin/sshd -d
 
 # Install MajorDoMo
 cd /root
@@ -14,3 +13,5 @@ cp /var/www/localhost/htdocs/config.php.sample /var/www/localhost/htdocs/config.
 chown -R apache:apache /var/www/localhost/htdocs
 find /var/www/localhost/htdocs -type f -exec sudo chmod 0666 {} \;
 find /var/www/localhost/htdocs -type d -exec sudo chmod 0777 {} \;
+
+/usr/sbin/sshd -d
